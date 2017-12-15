bd03c4fab4823e5b641313b173a9e13f984f5dd0
Joel Edwards <joeledwards@gmail.com>
2017-12-15 15:15:18 -0700

    feat: added standard and updated code to comply

--- bin/npm2es.js
+++ bin/npm2es.js
@@ -8,12 +8,12 @@ const argv = require('yargs')
   })
   .option('es', {
     describe: 'ElasticSearch to populate with search data',
-    default: process.env.ELASTIC_SEARCH ? `http://${process.env.ELASTIC_SEARCH}`: undefined,
+    default: process.env.ELASTIC_SEARCH ? `http://${process.env.ELASTIC_SEARCH}` : undefined,
     demand: true
   })
   .option('queueDepth', {
     describe: 'Max number of docs in the backlog',
-    default: process.env.QUEUE_DEPTH || 2048,
+    default: process.env.QUEUE_DEPTH || 2048
   })
   .option('since', {
     describe: 'Sequence # in couch to begin indexing from',
@@ -36,19 +36,19 @@ const argv = require('yargs')
     default: process.env.LEADER_SEQUENCE_POLL_FREQUENCY || 10000
   })
   .help()
-  .argv;
+  .argv
 
-const follow = require('follow'),
-    normalize = require('npm-normalize'),
-    request = require('request'),
-    Emitter = require('numbat-emitter'),
-    procMetrics = require('numbat-process'),
-    Monitor = require('micro-monitor'),
-    extend = require('extend'),
-    interval = argv.interval || 1000,
-    seqUrl = argv.es + '/config/sequence',
-    since = argv.since,
-    Queue = require('async').queue;
+const follow = require('follow')
+const normalize = require('npm-normalize')
+const request = require('request')
+const Emitter = require('numbat-emitter')
+const procMetrics = require('numbat-process')
+const Monitor = require('micro-monitor')
+const extend = require('extend')
+const interval = argv.interval || 1000
+const seqUrl = argv.es + '/config/sequence'
+const Queue = require('async').queue
+let since = argv.since
 
 // Configure only if a sink is supplied
 if (argv.metrics) {
@@ -62,37 +62,35 @@ if (argv.metrics) {
 
 if (typeof since === 'undefined') {
   request.get({
-    url : seqUrl,
+    url: seqUrl,
     json: true
-  }, function(e, r, o) {
+  }, function (e, r, o) {
     if (!r) {
-      return console.error('ERROR:', 'could not connect to elasticsearch (' + argv.es + ')');
+      return console.error('ERROR:', 'could not connect to elasticsearch (' + argv.es + ')')
     }
 
     if (!e && o && o._source && o._source.value) {
-      since = o._source.value;
+      since = o._source.value
     } else {
-      since = 0;
+      since = 0
     }
-    beginFollowing();
-  });
-
+    beginFollowing()
+  })
 } else {
   request.put({
-    url : seqUrl,
-    json : {
+    url: seqUrl,
+    json: {
       value: since
     }
-  }, function(e, r, o) {
-
+  }, function (e, r, o) {
     if (e) {
-      throw e;
+      throw e
     }
-    beginFollowing();
-  });
+    beginFollowing()
+  })
 }
 
-function trackSequence({couch, metricsReportFrequency, leaderSequencePollFrequency}) {
+function trackSequence ({couch, metricsReportFrequency, leaderSequencePollFrequency}) {
   let followerSequence = 0
   let leaderSequence = 0
   const sequenceOffset = () => leaderSequence - followerSequence
@@ -137,69 +135,67 @@ function trackSequence({couch, metricsReportFrequency, leaderSequencePollFrequen
   }
 }
 
-function beginFollowing() {
+function beginFollowing () {
   var settings = {
     analysis: {
       filter: {
         worddelimiter: {
-          type: "word_delimiter",
+          type: 'word_delimiter',
           preserve_original: true
         }
       },
       analyzer: {
-        "letter_analyzer": {
-          tokenizer: "letter",
-          filter: ["worddelimiter"]
+        'letter_analyzer': {
+          tokenizer: 'letter',
+          filter: ['worddelimiter']
         }
       }
     }
-  };
+  }
 
   request.put({
     url: argv.es,
     json: settings
-  }, function(e) {
-    if (e) return console.error('ERROR: could not put settings into elasticsearch ', e);
+  }, function (e) {
+    if (e) return console.error('ERROR: could not put settings into elasticsearch ', e)
 
     request.get({
       url: argv.es + '/package/_mapping',
       json: true
-    }, function(e, r, o) {
-
+    }, function (e, r, o) {
       var nameObj = {
-        type: "multi_field",
-        fields : {
-          name : { type : "string", index : "analyzed" },
-          untouched : { type : "string", index : "not_analyzed" }
+        type: 'multi_field',
+        fields: {
+          name: { type: 'string', index: 'analyzed' },
+          untouched: { type: 'string', index: 'not_analyzed' }
         }
-      };
+      }
 
       if (!e && !o.error && o.properties) {
         o['package'].properies.name = nameObj
       } else {
         o = {
-          "package" : {
-            "_all" : {
-              "index_analyzer": "letter_analyzer"
+          'package': {
+            '_all': {
+              'index_analyzer': 'letter_analyzer'
             },
-            properties : {
+            properties: {
               name: nameObj
             }
           }
-        };
+        }
       }
 
       request.put({
-        url : argv.es + '/package/_mapping',
-        json : o
-      }, function() {})
+        url: argv.es + '/package/_mapping',
+        json: o
+      }, function () {})
+    })
 
-    });
+    console.log('BEGIN FOLLOWING @', since)
 
-    console.log('BEGIN FOLLOWING @', since);
-
-    var last = since,
-      queue = _createThrottlingQueue(last, 16);
+    const last = since
+    const queue = _createThrottlingQueue(last, 16)
 
     const updateSequence = trackSequence(argv)
 
@@ -207,19 +203,19 @@ function beginFollowing() {
       db: argv.couch,
       since: since,
       include_docs: true
-    },  function(err, change) {
-      var _this = this;
+    }, function (err, change) {
+      var _this = this
 
       if (err) {
-        return console.error('ERROR:', err.message, argv.couch);
+        return console.error('ERROR:', err.message, argv.couch)
       }
 
       if (!change) {
-        return;
+        return
       }
 
       if (!change.id) {
-        return console.log('SKIP', change);
+        return console.log('SKIP', change)
       }
 
       // only allow N items into
@@ -228,73 +224,69 @@ function beginFollowing() {
       // created which fills memory.
       if (queue.length() < argv.queueDepth) {
         updateSequence(change.seq)
-        queue.push(change);
+        queue.push(change)
       } else {
-        this.pause();
-        queue.drain = function() {
-          _this.resume();
+        this.pause()
+        queue.drain = function () {
+          _this.resume()
         }
       }
-
-    });
-  });
+    })
+  })
 }
 
 // Create a queue that only allows concurrency
 // indexing operations to occur in parallel.
-function _createThrottlingQueue(last, concurrency) {
-  var queue = Queue(function(change, callback) {
-
+function _createThrottlingQueue (last, concurrency) {
+  var queue = Queue(function (change, callback) {
     if (last + interval < change.seq) {
-      last = change.seq;
+      last = change.seq
       request.put({
-        url : seqUrl,
-        json : {
+        url: seqUrl,
+        json: {
           value: last
         }
-      }, function(e, r, o) {
+      }, function (e, r, o) {
         if (e) {
-          console.error('ERROR', 'could not save latest sequence', e.message);
-          return callback();
+          console.error('ERROR', 'could not save latest sequence', e.message)
+          return callback()
         }
 
-        console.log('SYNC', last);
-      });
+        console.log('SYNC', last)
+      })
     }
 
     // Remove the document from elasticsearch
     if (change.deleted) {
-      request.del(argv.es + '/package/package/' + encodeURIComponent(change.id), function(err) {
+      request.del(argv.es + '/package/package/' + encodeURIComponent(change.id), function (err) {
         if (!err) {
-          console.log('DELETED', change.id);
+          console.log('DELETED', change.id)
         } else {
-          console.error('ERROR', 'could not delete document', err);
+          console.error('ERROR', 'could not delete document', err)
         }
-        callback();
-      });
+        callback()
+      })
 
     // Add the document to elasticsearch
     } else {
-
-      var p = normalize(change.doc);
+      var p = normalize(change.doc)
 
       if (!p || !p.name) {
-        console.log('SKIP: ' + change.doc._id);
-        return callback();
+        console.log('SKIP: ' + change.doc._id)
+        return callback()
       }
 
-      p.stars = p && p.users ? p.users.length : 0;
+      p.stars = p && p.users ? p.users.length : 0
 
       request({
         uri: 'https://api.npmjs.org/downloads/point/last-day/' + p.name,
         json: true,
         method: 'GET'
       }, function (e, r, bd) {
-
         if (e || bd.error) {
-          console.error(e ? e.message : bd.error, p);
+          console.error(e ? e.message : bd.error, p)
         } else {
-          p.dlDay = bd.downloads;
+          p.dlDay = bd.downloads
         }
 
         // get download counts for the last week
@@ -304,9 +296,9 @@ function _createThrottlingQueue(last, concurrency) {
           method: 'GET'
         }, function (e, r, bw) {
           if (e || bw.error) {
-            console.error(e ? e.message : bw.error, p);
+            console.error(e ? e.message : bw.error, p)
           } else {
-            p.dlWeek = bw.downloads;
+            p.dlWeek = bw.downloads
           }
 
           // get download counts for the last month
@@ -316,34 +308,33 @@ function _createThrottlingQueue(last, concurrency) {
             method: 'GET'
           }, function (e, r, bm) {
             if (e || bm.error) {
-              console.error(e ? e.message : bm.error, p);
+              console.error(e ? e.message : bm.error, p)
             } else {
-              p.dlMonth = bm.downloads;
+              p.dlMonth = bm.downloads
             }
 
             // use that information to get a sense of trending popularity
-            p.dlScore = p.dlWeek / (p.dlMonth / 4);
+            p.dlScore = p.dlWeek / (p.dlMonth / 4)
 
             request.get({
               url: argv.es + '/package/package/' + encodeURIComponent(p.name),
               json: true
-            }, function(e,b, obj) {
-
+            }, function (e, b, obj) {
               // follow gives us an update of the same document 2 times
               // 1) for the actual package.json update
               // 2) for the tarball
               // skip a re-index for #2
               if (!e && obj && obj._source && obj._source.version === p.version) {
-                console.log('SKIP VERSION:', change.doc._id, p.version, 'queue backlog = ', queue.length());
-                callback();
+                console.log('SKIP VERSION:', change.doc._id, p.version, 'queue backlog = ', queue.length())
+                callback()
               } else {
-                obj = obj || {};
-                //get numberof dev
-                if (p.dependencies){
-                  p.numberOfDependencies = p.dependencies.length;
+                obj = obj || {}
+                // get numberof dev
+                if (p.dependencies) {
+                  p.numberOfDependencies = p.dependencies.length
                 }
-                if (p.devDependencies){
-                  p.numberOfDevDependencies = p.devDependencies.length;
+                if (p.devDependencies) {
+                  p.numberOfDevDependencies = p.devDependencies.length
                 }
                 // modern versions of ES don't allow
                 // for keys with '.' in them.
@@ -357,25 +348,23 @@ function _createThrottlingQueue(last, concurrency) {
                 request.put({
                   url: argv.es + '/package/package/' + encodeURIComponent(p.name),
                   json: extend(obj._source || {}, p)
-                }, function(e, r, b) {
+                }, function (e, r, b) {
                   if (e) {
-                    console.error(e.message, p);
+                    console.error(e.message, p)
                   } else if (b.error) {
-                    console.error(b.error, p);
+                    console.error(b.error, p)
                   } else {
-                    console.log('ADD', p.name, 'status = ', r.statusCode, 'queue backlog = ', queue.length());
+                    console.log('ADD', p.name, 'status = ', r.statusCode, 'queue backlog = ', queue.length())
                   }
-                  callback();
-                });
+                  callback()
+                })
               }
-            });
-          });
-        });
-
-      });
+            })
+          })
+        })
+      })
     }
+  }, concurrency)
 
-  }, concurrency);
-
-  return queue;
+  return queue
 }
